0081639a17c9a0c254eb6bdf1675d66bd985fbeb
PauloAmora <pauloamora1111@gmail.com>
2017-11-08 12:16:40 -0300

    Formatting files

--- src/common/init.cpp
+++ src/common/init.cpp
@@ -47,7 +47,8 @@ void PelotonInit::Initialize() {
   // start epoch.
   concurrency::EpochManagerFactory::GetInstance().StartEpoch();
 
-  //logging::DurabilityFactory::Configure(LoggingType::ON, CheckpointType::CHECKPOINT_TYPE_INVALID, TimerType::TIMER_OFF);
+  // logging::DurabilityFactory::Configure(LoggingType::ON,
+  // CheckpointType::CHECKPOINT_TYPE_INVALID, TimerType::TIMER_OFF);
 
   // start GC.
   gc::GCManagerFactory::GetInstance().StartGC();
@@ -83,8 +84,7 @@ void PelotonInit::Initialize() {
   txn_manager.CommitTransaction(txn);
   logging::WalLogManager::SetDirectories({"/tmp/log"});
   logging::WalLogManager::DoRecovery();
-  //log_manager.StartLoggers();
-
+  // log_manager.StartLoggers();
 }
 
 void PelotonInit::Shutdown() {
